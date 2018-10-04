bc1be384cd71ab6518a56567c55803dfdb40006f
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-04 19:16:28 -0400

    tweaked for client in install_nfs_head.sh
    Added:
    sleep 50000000000 && mount -t nfs 192.168.1.3:/scratch /mnt/scratch
    sudo echo "192.168.1.3:/scratch /mnt  nfs defaults 0 0" >> /etc/fstab
    -Ben Walker

--- install_NFS_head.sh
+++ install_NFS_head.sh
@@ -6,3 +6,5 @@ chkconfig --level 35 portmap on
 chkconfig --level 35 nfs on
 sudo mkdir /software
 sudo echo"/software *(ro,sync,no_root_squash)" > /etc/exports 
+sleep 50000000000 && mount -t nfs 192.168.1.3:/scratch /mnt/scratch
+sudo echo "192.168.1.3:/scratch /mnt  nfs defaults 0 0" >> /etc/fstab
