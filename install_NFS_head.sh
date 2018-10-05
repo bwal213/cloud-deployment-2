7cdc04fb119e099089a31c35a8c2fb552b86d1f2
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-05 18:23:32 -0400

    Update install_NFS_head.sh
    Updated commands for mounting, turning on firewall, and exporting.

--- install_NFS_head.sh
+++ install_NFS_head.sh
@@ -10,11 +10,14 @@ sudo systemctl start rpcbind
 sudo systemctl start nfs-server
 sudo systemctl start nfs-lock
 sudo systemctl start nfs-idmap
-sudo exportfs -p /var/software 192.168.1.*(rw,sync,no_root_squash) 
+#sudo exportfs -p /var/software 192.168.1.*(rw,sync,no_root_squash)
+sudo exportfs *:/var/software -o rw,sync,no_root_squash
 sudo exportfs -ra
+sudo systemctl start firewalld.service
 sudo firewall-cmd --permanent --zone=public --add-service=nfs
 sudo firewall-cmd --permanent --zone=public --add-service=mountd
 sudo firewall-cmd --permanent --zone=public --add-service=rpc-bind
 sudo firewall-cmd --reload
-sleep 50000000000 && mount -t nfs 192.168.1.3:/var/scratch /mnt/scratch
+#sleep 50000000000 && mount -t nfs 192.168.1.3:/var/scratch /mnt/scratch
+sleep 50000000000 && sudo exportfs *:/var/software -o rw,sync,no_root_squash
 sudo echo "192.168.1.3:/var/scratch /mnt  nfs defaults 0 0" >> /etc/fstab
