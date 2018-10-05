888bfa630098e867837fc35ad3292eb2b9bfa87d
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-05 00:18:38 -0400

    Fixed an old rspec reference in profile.py.
    Missed a reference to rspec, has been updated to pg.

--- profile.py
+++ profile.py
@@ -53,7 +53,7 @@ for i in range( params.n ):
     
     # Specify the component id and the IPv4 address
   iface.component_id = "eth1"
-  iface.addAddress(rspec.IPv4Address("192.168.1." + str( i + 1 ), "255.255.255.0"))
+  iface.addAddress(pg.IPv4Address("192.168.1." + str( i + 1 ), "255.255.255.0"))
 
   link.addInterface(iface)
 
