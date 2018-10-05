a1061eddd5212429d2fb6848463aacaf20b4b346
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-05 17:36:48 -0400

    Update install_NFS_client.sh
    Updated commands so they use the better commands.

--- install_NFS_client.sh
+++ install_NFS_client.sh
@@ -1,10 +1,7 @@
-sudo yum -y install nfs-utils nfs-utils-lib
-sudo yum -y install portmap
-/etc/init.d/portmap start
-/etc/init.d/nfs start
-chkconfig --level 35 portmap on
-chkconfig --level 35 nfs on
-mount -t nfs 192.168.1.1:/software /mnt/software
-mount -t nfs 192.168.1.3:/scratch /mnt/scratch
-sudo echo "192.168.1.1:/software /mnt  nfs defaults 0 0" >> /etc/fstab
-sudo echo "192.168.1.3:/scratch /mnt  nfs defaults 0 0" >> /etc/fstab
+sudo yum -y install nfs-utils
+sudo mkdir -p /mnt/software
+sudo mkdir -p /mnt/scratch
+sudo mount -t nfs 192.168.1.1:/var/software /mnt/software
+sudo mount -t nfs 192.168.1.3:/var/scratch /mnt/scratch
+sudo echo "192.168.1.1:/var/software /mnt  nfs defaults 0 0" >> /etc/fstab
+sudo echo "192.168.1.3:/var/scratch /mnt  nfs defaults 0 0" >> /etc/fstab
