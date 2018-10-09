3de2c124261b16a708fcbae960df0d191e18fc7b
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-09 16:41:45 -0400

    Update install_NFS_head.sh
    Removed firewall commands and tweaked file locations

--- install_NFS_head.sh
+++ install_NFS_head.sh
@@ -1,24 +1,11 @@
 sudo yum -y install nfs-utils
-sudo mkdir /var/software
-sudo chmod -R 755 /var/software
-sudo chown nfsnobody:nfsnobody /var/software
-sudo systemctl enable rpcbind
-sudo systemctl enable nfs-server
-sudo systemctl enable nfs-lock
-sudo systemctl enable nfs-idmap
-sudo systemctl start rpcbind
-sudo systemctl start nfs-server
-sudo systemctl start nfs-lock
-sudo systemctl start nfs-idmap
-sudo systemctl start firewalld.service
-sudo firewall-cmd --permanent --zone=public --add-service=nfs
-sudo firewall-cmd --permanent --zone=public --add-service=mountd
-sudo firewall-cmd --permanent --zone=public --add-service=rpc-bind
-sudo firewall-cmd --reload
+sudo mkdir /mnt/software
+sudo chmod -R 755 /mnt/software
+sudo chown nfsnobody:nfsnobody /mnt/software
 #sudo exportfs -p /var/software 192.168.1.*(rw,sync,no_root_squash)
-sudo exportfs *:/var/software -o rw,sync,no_root_squash
+sudo exportfs *:/mnt/software -o rw,sync,no_root_squash
 sudo mkdir -p /mnt/scratch
 #sudo exportfs -ra
 #sleep 50000000000 && mount -t nfs 192.168.1.3:/var/scratch /mnt/scratch
-sleep 50s && sudo mount -t nfs 192.168.1.3:/var/scratch /mnt/scratch
-sudo echo "192.168.1.3:/var/scratch /mnt  nfs defaults 0 0" >> /etc/fstab
+sleep 30s && sudo mount -t nfs 192.168.1.3:/mnt/scratch /mnt/scratch
+sudo echo "192.168.1.3:/mnt/scratch /mnt/scratch  nfs defaults 0 0" >> /etc/fstab
