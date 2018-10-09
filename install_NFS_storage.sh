3be6d076045ef60aed33ed6072a200c259632734
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-09 16:44:26 -0400

    Update install_NFS_storage.sh
    Removed firewall commands and tweaked file locations.

--- install_NFS_storage.sh
+++ install_NFS_storage.sh
@@ -1,18 +1,6 @@
 sudo yum -y install nfs-utils
-sudo mkdir /var/scratch
-sudo chmod -R 755 /var/scratch
+sudo mkdir /mnt/scratch
+sudo chmod -R 755 /mnt/scratch
 sudo chown nfsnobody:nfsnobody /var/scratch
-sudo systemctl enable rpcbind
-sudo systemctl enable nfs-server
-sudo systemctl enable nfs-lock
-sudo systemctl enable nfs-idmap
-sudo systemctl start rpcbind
-sudo systemctl start nfs-server
-sudo systemctl start nfs-lock
-sudo systemctl start nfs-idmap
-sudo exportfs -p /var/scratch 192.168.1.*(rw,sync,no_root_squash) 
+sudo exportfs -p /mnt/scratch 192.168.1.*(rw,sync,no_root_squash) 
 sudo exportfs -ra
-sudo firewall-cmd --permanent --zone=public --add-service=nfs
-sudo firewall-cmd --permanent --zone=public --add-service=mountd
-sudo firewall-cmd --permanent --zone=public --add-service=rpc-bind
-sudo firewall-cmd --reload
