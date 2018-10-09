c999a185ed8c8d2a238d0e3d39c99bf9ba8ca549
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-09 16:50:32 -0400

    Update install_NFS_storage.sh
    Updated export command to a known working version. exportfs -ra still needs verification.

--- install_NFS_storage.sh
+++ install_NFS_storage.sh
@@ -2,5 +2,6 @@ sudo yum -y install nfs-utils
 sudo mkdir /mnt/scratch
 sudo chmod -R 755 /mnt/scratch
 sudo chown nfsnobody:nfsnobody /var/scratch
-sudo exportfs -p /mnt/scratch 192.168.1.*(rw,sync,no_root_squash) 
+#sudo exportfs -p /mnt/scratch 192.168.1.*(rw,sync,no_root_squash) 
+sudo exportfs *:/mnt/software -o rw,sync,no_root_squash
 sudo exportfs -ra
