6e7a23809a811699711224a6d2869f18606bf246
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-09 16:51:02 -0400

    Update install_NFS_storage.sh
    fixed a file location.

--- install_NFS_storage.sh
+++ install_NFS_storage.sh
@@ -1,7 +1,7 @@
 sudo yum -y install nfs-utils
 sudo mkdir /mnt/scratch
 sudo chmod -R 755 /mnt/scratch
-sudo chown nfsnobody:nfsnobody /var/scratch
+sudo chown nfsnobody:nfsnobody /mnt/scratch
 #sudo exportfs -p /mnt/scratch 192.168.1.*(rw,sync,no_root_squash) 
 sudo exportfs *:/mnt/software -o rw,sync,no_root_squash
 sudo exportfs -ra
