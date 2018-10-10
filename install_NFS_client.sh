ec192046f1dc0a89e811fe07ebf7af0e84ad31b5
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-10 15:23:05 -0400

    Update install_NFS_client.sh
    Updated commands that should edit fstab. Removed old duplicate commented out commands.

--- install_NFS_client.sh
+++ install_NFS_client.sh
@@ -1,9 +1,7 @@
 sudo yum -y install nfs-utils
 sudo mkdir -p /mnt/software
 sudo mkdir -p /mnt/scratch
-##sudo mount -t nfs 192.168.1.1:/var/software /mnt/software
 sudo mount -t nfs 192.168.1.1:/mnt/software /mnt/software
-##sudo mount -t nfs 192.168.1.3:/var/scratch /mnt/scratch
 sudo mount -t nfs 192.168.1.3:/mnt/scratch /mnt/scratch
-sudo echo "192.168.1.1:/mnt/software /mnt  nfs defaults 0 0" >> /etc/fstab
-sudo echo "192.168.1.3:/mnt/scratch /mnt  nfs defaults 0 0" >> /etc/fstab
+sudo echo "192.168.1.1:/mnt/software /mnt  nfs defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "192.168.1.3:/mnt/scratch /mnt  nfs defaults 0 0" | sudo tee -a /etc/fstab
