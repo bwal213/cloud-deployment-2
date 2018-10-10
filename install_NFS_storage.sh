cfec1fff3c2ae3411ba9dcc1a506b1280241a25e
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-10 15:19:07 -0400

    Update install_NFS_storage.sh
    Updated to proper/working /etc/exports edit command. Also enabled exportfs -ra, this edits other files and might do more work but makes sure that exports is updated and exported.

--- install_NFS_storage.sh
+++ install_NFS_storage.sh
@@ -3,5 +3,6 @@ sudo mkdir /mnt/scratch
 sudo chmod -R 755 /mnt/scratch
 sudo chown nfsnobody:nfsnobody /mnt/scratch
 #sudo exportfs -p /mnt/scratch 192.168.1.*(rw,sync,no_root_squash) 
-sudo exportfs *:/mnt/software -o rw,sync,no_root_squash
+#sudo exportfs *:/mnt/software -o rw,sync,no_root_squash
+sudo echo '/mnt/scratch *(rw,sync,no_root_squash)' | sudo tee /etc/exports
 sudo exportfs -ra
