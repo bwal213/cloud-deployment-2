07ced41ab81754a089f51155ca1bcc842eb049c9
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-10-10 15:27:53 -0400

    Update install_NFS_head.sh
    Added correct command for editing /etc/exports. Re enabled exportfs -ra. Removed legacy commands. Edited delay for scratch mount.

--- install_NFS_head.sh
+++ install_NFS_head.sh
@@ -2,10 +2,8 @@ sudo yum -y install nfs-utils
 sudo mkdir /mnt/software
 sudo chmod -R 755 /mnt/software
 sudo chown nfsnobody:nfsnobody /mnt/software
-#sudo exportfs -p /var/software 192.168.1.*(rw,sync,no_root_squash)
-sudo exportfs *:/mnt/software -o rw,sync,no_root_squash
+sudo echo '/mnt/software *(ro,sync,no_root_squash)' | sudo tee /etc/exports
 sudo mkdir -p /mnt/scratch
-#sudo exportfs -ra
-#sleep 50000000000 && mount -t nfs 192.168.1.3:/var/scratch /mnt/scratch
-sleep 30s && sudo mount -t nfs 192.168.1.3:/mnt/scratch /mnt/scratch
-sudo echo "192.168.1.3:/mnt/scratch /mnt/scratch  nfs defaults 0 0" >> /etc/fstab
+sudo exportfs -ra
+sleep 180s && sudo mount -t nfs 192.168.1.3:/mnt/scratch /mnt/scratch
+sudo echo "192.168.1.3:/mnt/scratch /mnt/scratch  nfs defaults 0 0" | sudo tee -a /etc/fstab
